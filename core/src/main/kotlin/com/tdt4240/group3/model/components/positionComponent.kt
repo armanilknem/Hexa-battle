f40b97415e3d71cba4c7eb4fd0dc562998a062ee
Peder Lillelien <peder.lillelien@gmail.com>
2026-03-19 15:18:45 +0100

    deleted positionComponent.kt

--- core/src/main/kotlin/com/tdt4240/group3/model/components/positionComponent.kt
+++
@@ -1,7 +0,0 @@
-package com.tdt4240.group3.model.components
-
-import com.badlogic.ashley.core.Component
-import com.badlogic.gdx.utils.Pool
-
-class PositionComponent: Component, Pool.Poolable {
-}
