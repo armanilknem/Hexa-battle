1df9b20a2f78f1909ce71440758d8bbaeee23829
vhbell <victoria@bell.as>
2026-03-19 14:28:13 +0100

    feat: add function createCity in EntityFactory.kt

--- core/src/main/kotlin/com/tdt4240/group3/model/entities/EntityFactory.kt
+++ core/src/main/kotlin/com/tdt4240/group3/model/entities/EntityFactory.kt
@@ -2,10 +2,11 @@ package com.tdt4240.group3.model.entities
 
 import com.tdt4240.group3.model.components.PlayerComponent
 import com.badlogic.ashley.core.Engine
+import com.tdt4240.group3.model.components.CityComponent
+import com.tdt4240.group3.model.components.PositionComponent
+import com.tdt4240.group3.model.components.Team ///NOO
 import ktx.ashley.entity
 import ktx.ashley.with
-
-
 
 class EntityFactory(private val engine: Engine) {
 
@@ -15,5 +16,16 @@ class EntityFactory(private val engine: Engine) {
         }
     }
 
-
+    fun createCity(name: String, team: Team, isCapital: Boolean, baseProduction: Int, x: Int, y: Int) = engine.entity {
+        with<CityComponent> {
+            this.name = name
+            this.team = team
+            this.baseProduction = baseProduction
+            this.isCapital = isCapital
+        }
+        with<PositionComponent> {
+            this.x = x
+            this.y = y
+        }
+    }
 }
