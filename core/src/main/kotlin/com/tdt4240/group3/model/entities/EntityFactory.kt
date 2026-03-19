0eeff4ac25a07f83e1354d140a5ec219e3fe395a
vhbell <victoria@bell.as>
2026-03-19 14:44:04 +0100

    feat: add teamcomponent to createCity

--- core/src/main/kotlin/com/tdt4240/group3/model/entities/EntityFactory.kt
+++ core/src/main/kotlin/com/tdt4240/group3/model/entities/EntityFactory.kt
@@ -4,7 +4,7 @@ import com.tdt4240.group3.model.components.PlayerComponent
 import com.badlogic.ashley.core.Engine
 import com.tdt4240.group3.model.components.CityComponent
 import com.tdt4240.group3.model.components.PositionComponent
-import com.tdt4240.group3.model.components.Team ///NOO
+import com.tdt4240.group3.model.components.TeamComponent
 import ktx.ashley.entity
 import ktx.ashley.with
 
@@ -16,16 +16,18 @@ class EntityFactory(private val engine: Engine) {
         }
     }
 
-    fun createCity(name: String, team: Team, isCapital: Boolean, baseProduction: Int, x: Int, y: Int) = engine.entity {
+    fun createCity(name: String, isCapital: Boolean, baseProduction: Int, x: Int, y: Int, team: TeamComponent.TeamName) = engine.entity {
         with<CityComponent> {
             this.name = name
-            this.team = team
             this.baseProduction = baseProduction
             this.isCapital = isCapital
         }
         with<PositionComponent> {
             this.x = x
             this.y = y
+        }
+        with<TeamComponent> {
+            this.team = TeamComponent.TeamName.NONE
         }
     }
 }
