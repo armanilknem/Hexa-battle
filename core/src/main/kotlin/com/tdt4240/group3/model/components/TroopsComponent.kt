a6890e0c3de2d28542fbaced7cc70ba877ccf14e
Andrea Stromstad <andrea.stromstad@outlook.com>
2026-03-21 23:35:11 +0100

    fix: remove position in Troops and add reset to tileComp

--- core/src/main/kotlin/com/tdt4240/group3/model/components/TroopsComponent.kt
+++ core/src/main/kotlin/com/tdt4240/group3/model/components/TroopsComponent.kt
@@ -7,18 +7,12 @@ import ktx.ashley.mapperFor
 enum class Team {RED, GREEN, BLUE, PURPLE}
 
 class TroopsComponent : Component, Pool.Poolable {
-    var positionX: Int = 0
-    var positionY: Int = 0
-    var positionZ: Int = 0
     var strength: Int = 0
     var isMoved: Boolean = false
     var isClicked: Boolean = false
     var team: Team = Team.RED
 
     override fun reset() {
-        positionX = 0
-        positionY = 0
-        positionZ = 0
         strength = 0
         isMoved = false
         isClicked = false
