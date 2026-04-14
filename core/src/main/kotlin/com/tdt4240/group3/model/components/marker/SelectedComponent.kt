013a4a960a63243ff3e1ae67454eee501408dfe5
vhbell <victoria@bell.as>
2026-04-14 09:44:40 +0200

    feat: create marker components

---
+++ core/src/main/kotlin/com/tdt4240/group3/model/components/marker/SelectedComponent.kt
@@ -0,0 +1,8 @@
+package com.tdt4240.group3.model.components.marker
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
+
+class SelectedComponent : Component, Pool.Poolable {
+    override fun reset() = Unit
+}
