553848b37d2f35dd2f1d3e7b79b4164f662ff315
Andrea Stromstad <andrea.stromstad@outlook.com>
2026-04-16 18:17:48 +0200

    refactor: selection logic

--- core/src/main/kotlin/com/tdt4240/group3/controller/PlayController.kt
+++ core/src/main/kotlin/com/tdt4240/group3/controller/PlayController.kt
@@ -33,7 +33,7 @@ class PlayController(
         val turnController = TurnController(turnSystem)
         val troopCreationController = TroopCreationController(troopCreationSystem)
         val pauseController = PauseController()
-        val selectionController = SelectionController(selectionSystem)
+        val selectionController = SelectionController(engine)
 
         this.setUpInitialGameState()
         this.setUpWorld()
